cbba6e5661839558cce7d434f8a955c65fd68219
mackandelius <fransson.mackan@gmail.com>
2025-07-03 02:49:54 +0200

    Debugging

--- games/spacestation14/entrypoint.sh
+++ games/spacestation14/entrypoint.sh
@@ -32,17 +32,25 @@ git submodule update --init --recursive
 
 #python RUN_THIS.py || /usr/bin/python3 RUN_THIS.py || /usr/bin/python RUN_THIS.py
 
+mkdir -p /mnt/server
+cd /mnt/server
+
 echo "Building server"
 dotnet build Content.Packaging --configuration Release
 dotnet run --project Content.Packaging server --hybrid-acz --platform linux-x64
 
+
 # Move compiled files to accessible server folder.
-#mkdir -p /mnt/server
-#mv release/ /mnt/server/
+mv ./release/ /mnt/server/
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# DEBUG
+ls
+find / -type f -name Robust.Server
+
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
